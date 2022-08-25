542f260fa69c5134c61fc5af7bc1a3e3bc6d79c4
Akkrapol Kanpong <aukkrapolarm@gmail.com>
2022-08-26 00:06:48 +0700

    Initial Project

---
+++ frontend/src/Components/Login/index.tsx
@@ -0,0 +1,20 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Image from "material-ui-image";
+
+export default function Login() {
+    return (
+
+        <Container>
+            <Box>
+                <Image
+                    src="https://avatars.githubusercontent.com/u/93438276?s=400&u=77beb13f5d89f02ea0b665b523e22572c6935c65&v=4"
+                    alt="TouchMyLike"
+                    loading="lazy"
+                />
+            </Box>
+
+        </Container>
+    )
+}
